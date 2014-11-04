558601fdd3790f55708136e5f668813586b7c597
Naomi Gaynor <naomi.gaynor@gmail.com>
2014-11-04 12:41:39 +0000

    server.js

--- hapiserver/server.js
+++ hapiserver/server.js
@@ -17,3 +17,23 @@ var dbOpts = {
             "native_parser": false
         }
     }
+
+pack.register(
+    {
+        plugin:require('hapi-mongodb'), 
+        options: dbOpts
+    },
+    function(err){
+        if (err){
+            onsole.log(err);
+            throw err;
+        }
+    });
+
+if(!module.parent){
+    pack.start(function(){
+        console.log("hapi server started @", server.info.uri);
+    });
+}
+
+server.route(routes)
