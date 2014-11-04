923ba065106bc6b857ee343c0c94ea24945ee71a
Natalie Newton <natali.newton523@gmail.com>
2014-11-04 12:18:14 +0000

    createed routes and handlers - functions still to be written for handlers

--- hapiserver/routes/routes.js
+++ hapiserver/routes/routes.js
@@ -2,11 +2,46 @@ var handlers = require("../handlers/handlers.js");
 
 module.exports = [
 	{
-		
+		path: '/',
+	    method: 'GET',
+	    handler: handlers.viewToDos
+
 	},
 
 	{
-		
+		path: '/addnew',
+	    method: 'GET',
+	    handler: handlers.newToDo
+	},
+
+	{
+		path: '/addnew',
+	    method: 'PUSH',
+	    handler: handlers.submitNew
+	},
+
+	{
+		path: '/edit',
+	    method: 'GET',
+	    handler: handlers.makeEdit
+	},
+
+	{
+		path: '/edit',
+	    method: 'PUSH',
+	    handler: handlers.submitEdit
+	},
+
+	{
+		path: '/delete',
+	    method: 'GET',
+	    handler: handlers.deleteToDo
+	},
+
+	{
+		path: '/delete',
+	    method: 'PUSH',
+	    handler: handlers.submitDelete
 	}
 
 ];
